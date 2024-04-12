57662787465649b3113ac98b30b6cd25a568dd48
SaumickPradhan <saumick.pradhan@gmail.com>
2024-04-12 00:57:01 -0400

    Add CoolClass member functions for calculating average, square, and prime numbers

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -39,3 +39,31 @@ int CoolClass::Ming (int c, int d)
         return c - d;
     }
 }
+
+
+float CoolClass::Average(float array[], int size)
+{
+    float sum = 0.0;
+    for (int i = 0; i < size; ++i)
+    {
+        sum += array[i];
+    }
+    return sum / size;
+}
+
+int CoolClass::Square(int x)
+{
+    return x * x;
+}
+
+bool CoolClass::IsPrime(int num)
+{
+    if (num <= 1)
+        return false;
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -44,5 +44,29 @@ namespace UnitTest1
 			CoolClass coolClass;
 			Assert::AreEqual(5, coolClass.Ming(3, 2));
 		}
-	};
+	TEST_METHOD(TestMethod6)
+        {
+            CoolClass coolClass;
+            float array[] = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+            Assert::AreEqual(3.0f, coolClass.Average(array, 5));
+        }
+
+        TEST_METHOD(TestMethod7)
+        {
+            CoolClass coolClass;
+            Assert::AreEqual(25, coolClass.Square(5));
+        }
+
+        TEST_METHOD(TestMethod8)
+        {
+            CoolClass coolClass;
+            Assert::IsTrue(coolClass.IsPrime(7));
+        }
+
+        TEST_METHOD(TestMethod9)
+        {
+            CoolClass coolClass;
+            Assert::IsFalse(coolClass.IsPrime(6));
+        }
+    };
 }
